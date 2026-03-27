3ae06f8d4dfa1a4eedafbcb0414b0051f33d4a4a
Severino Souza <severinosouza.ufam@gmail.com>
2026-03-27 16:02:20 -0400

    ex8

---
+++ cap04/nodejs/ex4_8.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")()
+
+const valor = Number(prompt("Total da Compra: "))
+const aux = Math.floor(valor /20)
+
+
+
+const parcelas = aux == 0? 1 : aux > 6 ? 6 : aux
+//ou let parcelas if(aux == 0) { parcelas = 1} else if (aux > 6 ) {parcelas = 6} else {parcelas = aux} 
+
+const valorParcela = valor / parcelas
+console.log(`Pode pagar em ${parcelas}x de R$: ${valorParcela.toFixed(2)}`)
