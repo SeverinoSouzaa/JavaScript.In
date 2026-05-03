629d374a898f1571d8e10a35d42d2ee889fc2e87
Severino Souza <severinosouza.ufam@gmail.com>
2026-05-03 16:03:22 -0400

    comentarios

--- cap07/js/ex7_8.js
+++ cap07/js/ex7_8.js
@@ -1,7 +1,7 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-
+//Define as taxas que o banco cobra.
 const TAXA_MULTA = 2 / 100
 const TAXA_JUROS = 0.33 / 100
 
@@ -11,15 +11,26 @@ frm.addEventListener("submit", (e) =>{
     const dataVenc = frm.inDataVenc.value
     const valor = Number(frm.inValor.value)
 
+    //tira uma foto do exato milisegundo em que linha roda
     const hoje = new Date()
     const vencto = new Date()
 
+    //corta toda vez que acha um traço
+    //assim criando um vetor ["2026", "05", "03"]
+    //[0]= ano, [1]= mes, [2] = dia
     const partes = dataVenc.split("-")
+
+    //ajusta o dia e pega o indice 2
     vencto.setDate(Number(partes[2]))
+
+    //são 12 meses, mas  começa em 0, tipo dez seria 11
+    //então colocar - 1 pega o mes correto para nós
     vencto.setMonth(Number(partes[1]) - 1)
+
+    //ajusta o ano e pega o indice 0
     vencto.setFullYear(Number(partes[0]))
 
-
+    
     const atraso = hoje - vencto
     let multa = 0
     let juros = 0
