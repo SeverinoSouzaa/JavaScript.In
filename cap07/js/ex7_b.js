b5fd6ea60d4452e5db4f165ab115e044723ebfd1
Severino Souza <severinosouza.ufam@gmail.com>
2026-05-04 16:11:09 -0400

    ajuste

--- cap07/js/ex7_b.js
+++ cap07/js/ex7_b.js
@@ -6,21 +6,20 @@ frm.addEventListener("submit", (e) => {
 
     const frase = frm.inFrase.value.toUpperCase()
 
-    const tam = frase.length
-
     const fraseLimpa = frase.replace(/ /g, "").toUpperCase()
+     const tam = fraseLimpa.length
 
     let inverso = fraseLimpa.charAt(tam - 1).toUpperCase()
 
     for (let i = tam - 2; i >= 0; i--) {
         inverso = inverso + fraseLimpa.charAt(i).toUpperCase()
+    }
 
-        if (inverso == fraseLimpa){
+     if (inverso == fraseLimpa){
             resp.innerText = `${frase} É um Palíndromo`
         } else {
             resp.innerText = `${frase} Não é um Palíndromo`
         }
 
-    }
 
 })
