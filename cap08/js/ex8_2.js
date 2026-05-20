efab12389cd782a066bd0b7847a5022dd863f355
Severino Souza <severinosouza.ufam@gmail.com>
2026-05-20 19:35:07 -0400

    comentarios

--- cap08/js/ex8_2.js
+++ cap08/js/ex8_2.js
@@ -3,7 +3,9 @@ const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
 const resp3 = document.querySelector("#outResp3")
 
-//função recebe o ano do veiculo como parâmetro
+//função que começa com o parametro "ano" como exemplo vazio, anoAtual olhamos para a data e pegamos o ano atual, depois fazemos uma condição para verificar se o nosso parametro vazio é igual ao anoAtual que pegamos ou outras condições
+
+//ano recebe então o ano que o usuario digitar no campo
 const classificarVeiculo = (ano) => {
     const anoAtual = new Date().getFullYear()
     let classif
@@ -19,16 +21,24 @@ const classificarVeiculo = (ano) => {
 
 const calcularEntrada = (valor, status) => status == "Novo" ? valor * 0.5 : valor * 0.3
 
+//recebendo do "const entrada" fica: valor: digitado pelo user, status: resultado da função classificação 
+
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    const modelo = frm.inModelo.value
-    const ano = Number(frm.inAno.value)
-    const preco = Number(frm.inPreco.value)
+    const modelo = frm.inModelo.value//le e guarda o modelo 
 
-    const classificacao = classificarVeiculo(ano)
-    const entrada = calcularEntrada(preco, classificacao)
+    const ano = Number(frm.inAno.value) //le  e guarda o ano que vamos usar na função
+
+    const preco = Number(frm.inPreco.value)
+    //le e guarda o preco
+
+    const classificacao = classificarVeiculo(ano) //Aqui recebe o valor da função classificar veiculo com o parametro vazio sendo preenchido pela variavel ano onde o user digitou 
+
+    const entrada = calcularEntrada(preco, classificacao)// Aqui recebe o valor da função calcularEntrada atribuindo aqueles parametros vazios o "preco" e "classificação"
+
+    
     const parcela = (preco - entrada) / 10
 
     resp1.innerText = modelo + " - " + classificacao
